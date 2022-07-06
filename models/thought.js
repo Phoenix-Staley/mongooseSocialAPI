35e1829534c6dc579244bbfa79a91aa58de13b11
Phoenix_Linux_Laptop <PhoenixStaley121@hotmail.com>
2022-07-06 12:08:41 -0700

    Add technical requirements

--- models/thought.js
+++ models/thought.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
 const reactionSchema = require("./reaction");
+const formatDate = require("../utils/formatDate");
+
 
 const thoughtSchema = new mongoose.Schema({
-    thoughtText: { type: String, max: 280, required: true },
-    createdAt: { type: Date, required: true },
+    // Validates that there is 1-280 characters
+    thoughtText: { type: String, validate: /.{1,}/, max: 280, required: true },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: formatDate
+    },
     username: { type: String, required: true, ref: "user" },
     reactions: [reactionSchema]
 });
 
-thoughtSchema.methods.reactionCount = function () {
+thoughtSchema.virtual("reactionCount").get(function () {
     return this.reactions.length;
-}
+});
 
 const Thought = new mongoose.model("thought", thoughtSchema);
 
