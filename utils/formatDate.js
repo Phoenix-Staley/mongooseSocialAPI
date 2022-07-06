3932fa1aafe98919dccab720821f18f2f5149ca1
Phoenix_Linux_Laptop <PhoenixStaley121@hotmail.com>
2022-07-06 12:09:03 -0700

    Add formatDate util

---
+++ utils/formatDate.js
@@ -0,0 +1,5 @@
+const formatDate = function () {
+    return this.createdAt.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+}
+
+module.exports = formatDate;
