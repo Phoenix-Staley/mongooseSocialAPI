5c2caa220622457645abea22672ad629e84e0b53
Phoenix_Linux_Laptop <PhoenixStaley121@hotmail.com>
2022-07-06 12:19:06 -0700

    Add comments, and unique restraints

--- models/user.js
+++ models/user.js
@@ -1,14 +1,28 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, max: 30, trim: true },
-    email: { type: String, required: true, validate: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        max: 30,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        // Ensures anything stored here is a valid email address
+        validate: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    // Stores any thoughts created by this user
     thoughts: [
         {
             type: Schema.Types.ObjectId,
             ref: "thought"
         }
     ],
+    // Stores any friends this user has
     friends: [
         {
             type: Schema.Types.ObjectId,
