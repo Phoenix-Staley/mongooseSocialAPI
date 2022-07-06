5c33d40ee24055b9bf52e832e7993483cf4ba8ec
Phoenix_Linux_Laptop <PhoenixStaley121@hotmail.com>
2022-07-06 10:31:30 -0700

    Implement Thought model

---
+++ models/thought.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const reactionSchema = require("./reaction");
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: { type: String, max: 280, required: true },
+    createdAt: { type: Date, required: true },
+    username: { type: String, required: true, ref: "user" },
+    reactions: [reactionSchema]
+});
+
+thoughtSchema.methods.reactionCount = function () {
+    return this.reactions.length;
+}
+
+const Thought = new mongoose.model("thought", reactionSchema);
+
+module.exports = Thought;
