5f184d0628311106004d31f2eabaebb79766f7bb
Phoenix_Linux_Laptop <PhoenixStaley121@hotmail.com>
2022-07-06 12:18:10 -0700

    Add default to createdAt property

--- models/reaction.js
+++ models/reaction.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+const formatDate = require("../utils/formatDate");
 
 const reactionSchema = new mongoose.Schema({
     reactionId: mongoose.Types.ObjectId,
     reactionBody: { type: String, max: 280, required: true },
     username: { type: String, required: true, ref: "user" },
-    createdAt: { type: Date, required: true }
+    createdAt: { type: Date, default: Date.now, get: formatDate }
 });
 
 module.exports = reactionSchema;
