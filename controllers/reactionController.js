59f82fcc104e3fe3b4bc9997d77ec65dda23efdb
Phoenix_Linux_Laptop <PhoenixStaley121@hotmail.com>
2022-07-08 10:02:10 -0700

    Remove app-crashing bug

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -14,11 +14,7 @@ module.exports = {
                 { _id: req.params.thoughtId },
                 { $pull: { reactions: { _id: req.body.reactionId } } }
             )
-            .then((updatedThoughtData) => {
-                !updatedThoughtData
-                    ? res.status(404).json({ message: "No user with that ID" })
-                    : res.json(res.status(201).json({ message: `Successfully deleted the reaction with ID ${req.body.reactionId}` }))
-            })
+            .then((updatedThoughtData) => res.status(201).json({ message: `Successfully deleted the reaction with ID ${req.body.reactionId}` }))
             .catch((err) => res.status(400).json(err));
     }
 }
