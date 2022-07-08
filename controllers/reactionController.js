b201b8b4ff46d384764f9beecb8ba2bfc045468e
Phoenix_Linux_Laptop <PhoenixStaley121@hotmail.com>
2022-07-08 09:58:33 -0700

    Fix controller not finding reaction

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -2,16 +2,19 @@ const Thought = require("../models/Thought");
 
 module.exports = {
     createReaction(req, res) {
-        Thought.findOneAndUpdate({ _id: req.params.thoughtId },
-            { $push: { reactions: req.body } })
-            .then((updatedThoughtData) => res.json(updatedThoughtData.reactions[-1]))
+        Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $push: { reactions: req.body } }
+            )
+            .then((updatedThoughtData) => res.status(201).json(updatedThoughtData))
             .catch((err) => res.status(400).json(err));
     },
     deleteReaction(req,res) {
-        Thought.findOneAndUpdate({ _id: req.params.thoughtId },
-            { $pull: { reactions: req.body.reactionId } }
-        )
-        .then((updatedThoughtData) => res.status(201).json({ message: `Successfully deleted the reaction with ID ${req.body.reactionId}` }))
-        .catch((err) => res.status(400).json(err));
+        Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $pull: { reactions: { _id: req.body.reactionId } } }
+            )
+            .then((updatedThoughtData) => res.status(201).json({ message: `Successfully deleted the reaction with ID ${req.body.reactionId}` }))
+            .catch((err) => res.status(400).json(err));
     }
 }
